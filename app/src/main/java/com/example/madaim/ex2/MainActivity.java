1e9732b60bd48a529518b6fec83cb243bfad1a2b
KaiOrland <kayorland@gmail.com>
2016-09-27 17:28:54 +0300

    saveinstance

--- app/src/main/java/com/example/madaim/ex2/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex2/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.madaim.ex2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int clickCount = 0;
 
     @Override
     protected void onDestroy() {
@@ -48,4 +53,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(myApp.MYTAG, "main:onRestoreInstance event");
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i(myApp.MYTAG, "main:onSaveInstance event");
+        super.onSaveInstanceState(outState);
+    }
+
+    public void displayMessage(View view){
+        clickCount++;
+        findViewById(R.id.btName);
+        EditText ed = (EditText)findViewById(R.id.editText);
+        String name = ed.getText().toString();
+        String message = getResources().getString(R.string.emptyName);
+        if(name.isEmpty()){
+            Toast.makeText(this,String.format(message, clickCount),Toast.LENGTH_SHORT).show();
+        }
+        else{
+            String message2 = getResources().getString(R.string.emptyName2);
+            Toast.makeText(this,String.format(message2, clickCount,name),Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
